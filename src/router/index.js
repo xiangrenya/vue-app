83cebc747e8f17e911725d33f046cd89d599edf9
项仁亚 <xiangrenya@gmail.com>
2022-06-24 15:32:32 +0800

    feat: 使用 leaflet 加载高德地图服务

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,11 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/map/amap",
+    name: "amap",
+    component: () => import("../views/map/AMap.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/map/AMap.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="amap"></div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+export default {
+  name: "AMap",
+  mounted() {
+    const map = L.map("amap");
+
+    L.tileLayer(
+      "http://webrd01.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8",
+      {
+        maxZoom: 24,
+      }
+    ).addTo(map);
+
+    map.setView([31.369328, 120.504931], 14);
+  },
+};
+</script>
+
+<style lang="scss">
+#amap {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
